c1ec9f5ecdd95e2bbeda83fd07b25a20ceffd6ff
Diego <diegosalischiker711@gmail.com>
2021-05-20 14:51:11 -0300

    Vuex Store
    Llamado a la MovieDB API con Axios para traer las películas más populares vía Action getAPIData que busca la URL con getters que traen la baseURL del state y la apiKey desde .env.
    En caso de éxito se llama a la mutation setMovies que guardan el resultado de la request en el state (movies[])

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,37 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import axios from "axios";
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+const store = new Vuex.Store({
+  state: {
+    baseURL: "https://api.themoviedb.org/3/movie/popular?api_key=",
+    movies: [],
+  },
+  mutations: {
+    setMovies(state, movies){
+      state.movies = movies;
+    },
+  },
+  actions: {
+    getAPIData({ commit, getters }) {
+      axios
+        .get(getters.getURL)
+        .then((response) => {
+          commit("setMovies", response.data.results);
+        })
+        .catch((error) => console.error(error));
+    },
+  },
+  getters: {
+    getURL: (state, getters) => {
+      return `${state.baseURL}${getters.getApiKey}&language=en-US&page=1`;
+    },
+    getApiKey: () => {
+      return process.env.VUE_APP_API_KEY;
+    },
+  },
   modules: {},
 });
+
+export default store;
