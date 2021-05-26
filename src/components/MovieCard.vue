dd0c3e2d9b38b674eb9059e1c01116292944b054
Diego <diegosalischiker711@gmail.com>
2021-05-26 18:16:08 -0300

    RouterLink a MovieSingle.vue y muestra de data de película
    - Llamado a la API desde MovieSingle.vue para que se traiga la data aún si no el usuario no viene de home y seleccionó película.
    - Si se ingresa directo url/movie/:id se hará llamado a la API enviando ese :id como parámetro.
    - Estilos momentáneos para poder ver la información.

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -7,13 +7,14 @@
     }"
   >
     <div class="movie-card__container" @click="selectMovie(movie.id)">
-      <h1 class="movie-card__title">{{ this.movie.title }}</h1>
-      <p class="movie-card__description">{{ this.movie.vote_average }}</p>
       <img
         class="movie-card__img"
         :src="`https://image.tmdb.org/t/p/w500${this.movie.poster_path}`"
         alt=""
-      /></div
+      />
+      <h3 class="movie-card__title">{{ this.movie.title }}</h3>
+      <p class="movie-card__description">{{ this.movie.vote_average }}</p>
+      </div
   ></RouterLink>
 </template>
 
@@ -25,7 +26,7 @@ export default {
   methods: {
     selectMovie(id) {
       this.$store.commit("setSelectedMovieId", id);
-      this.$store.dispatch("getMovieDataFromAPI");
+
     },
   },
   computed: {
@@ -37,6 +38,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.movie-card{
+  text-decoration: none;
+  max-width: 25%;
+}
+.movie-card__container{
+  margin: 0 auto;
+  max-width: 150px;
+  text-align: center;
+  color: #222222;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 .movie-card__img {
   width: 150px;
   height: auto;
--- src/views/MovieSingle.vue
+++ src/views/MovieSingle.vue
@@ -1,28 +1,63 @@
 <template>
   <div>
-    MOVIE SINGLE
-    {{ movieData.title }}
+    <div class="movie-single__container">
+      MOVIE SINGLE
+      <img
+        class="movie-card__img"
+        :src="`https://image.tmdb.org/t/p/w500${this.movieData.poster_path}`"
+        alt=""
+      />
+      <h1>{{ this.movieData.title }}</h1>
+      <p>{{ this.movieData.tagline }}</p>
+      <p>{{ this.movieData.vote_average }}</p>
+      <span
+        v-for="(language, $index) in this.movieData.spoken_languages"
+        :key="$index"
+        >{{ language.english_name }}</span
+      >
+      <span>{{ this.movieData.runtime }} MIN</span>
+      <span>{{ this.getReleaseYear() }}</span>
+      <h4>Synopsis</h4>
+      <p>{{ this.movieData.overview }}</p>
+      <h4>Genres</h4>
+      <ul>
+        <li v-for="(genre, $index) in this.movieData.genres" :key="$index">
+          {{ genre.name }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "MovieSingle",
-  props: ['id'],
-  data() {
-    return {
-      movieId: this.$route.params.id,
-      movies: this.$store.state.movies,
-    };
+  props: ["id"],
+  created() {
+    this.$store.commit("setSelectedMovieId", this.movieId);
+    this.$store.dispatch("getMovieDataFromAPI");
+  },
+  methods: {
+    getReleaseYear() {
+      if (this.movieData.release_date) {
+        const releaseYear = this.movieData.release_date.substring(0, 4);
+        return releaseYear;
+      }
+    },
   },
   computed: {
+    movieId() {
+      return this.$route.params.id;
+    },
     movieData() {
-      return this.movies.find(
-        (movie) => movie.id === this.movieId
-      );
+      return this.$store.state.selectedMovieData;
     },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.movie-single__container {
+  top: 10vh;
+}
+</style>
