99d4af3a32b210531ee1a2f6a415878312bb63c5
Diego <diegosalischiker711@gmail.com>
2021-05-26 18:12:12 -0300

    Estilos componente GenresList.vue
    Y agregado RouterLink a cada género de la lista que va a la ruta /genre/:id enviando el id del género por params

--- src/components/GenresList.vue
+++ src/components/GenresList.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="genres__container">
     <ul class="genres__ul">
-      <li class="genres__ul__li" v-for="(genre, $index) in genres" :key="$index">
-          <a class="genres__ul__li__a" href="#" to="/">{{genre.name}}</a>
-      </li>
+      <h3>Genres</h3>
+      <RouterLink
+        v-for="(genre, $index) in genres"
+        :key="$index"
+        class="genre__link"
+        :to="{
+          name: 'Genre',
+          params: { id: genre.id },
+        }"
+      >
+        <li class="genres__ul__li">
+          <a class="genres__ul__li__a" href="#" to="/">{{ genre.name }}</a>
+        </li>
+      </RouterLink>
     </ul>
   </div>
 </template>
@@ -23,30 +34,52 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.genres__container{
-    padding: .2em 0.5em;
-    background-color: #dbdbdb;
+.genres__container {
+  background-color: #dbdbdb;
+  width: 20%;
+  min-width: 250px;
+  height: 90vh;
+  top: 10vh;
+  position: -webkit-sticky;
+  position: sticky;
+  display: flex;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-weight: 600;
 }
-.genres__ul{
+.genres__ul {
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  h3 {
+    color: #474747;
+  }
+  .genre__link{
+    width: 100%;
+    text-decoration: none;
+  }
+  .genres__ul__li {
+    width: 100%;
     display: flex;
-    flex-flow:wrap;
-    row-gap: .5em;
+    flex-direction: column;
     justify-content: center;
-    .genres__ul__li{
-        list-style: none;
-        margin-right: .5em;
-        cursor: pointer;
-        background-color: white;
-        padding: 0.2rem .7rem;
-        border-radius: 18px;
-        &:hover{
-            background-color: #cccccc;
-        }
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    height: 1.7rem;
+    list-style: none;
+    cursor: pointer;
+
+    padding: 0rem 0.7rem;
+    &:hover {
+      background-color: #cccccc;
     }
-    .genres__ul__li__a{
-        font-size: 14px;
-        text-decoration: none;
-        color: #474747;
-    }
+  }
+  .genres__ul__li__a{
+    font-size: 12px;
+    text-decoration: none;
+    color: #474747;
+  }
 }
 </style>
