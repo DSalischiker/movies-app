59ce262bf33f89b92772951d56582a22648c9926
Diego <diegosalischiker711@gmail.com>
2021-05-28 17:58:15 -0300

    Breadcrumbs
    Código para añadir breadcrumbs debajo del header.
    Todavía no funcional así que comentado.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,25 +1,61 @@
 <template>
+<!-- <div> -->
   <header class="header">
     <h1 class="header__title">Movies List</h1>
     <nav class="header__nav">
       <RouterLink to="/" class="header__nav__link">Home</RouterLink>
     </nav>
   </header>
+  <!-- <div>
+    <ul>
+      <li
+      v-for="(breadcrumb, index) in breadcrumbList"
+      :key="index"
+      @click="routeTo(index)"
+      :class="{'linked': !!breadcrumb.link}">
+      {{breadcrumb.name}}
+      </li>
+    </ul>
+  </div>
+  </div> -->
 </template>
 
 <script>
 export default {
   name: "AppHeader",
+  /* mounted(){
+    this.updateList();
+  },
+  data(){
+    return {
+      breadcrumbList: [],
+    }
+  }, */
   methods: {
     selectView(name) {
       this.$emit("selectView", name);
     },
+    /* routeTo(pRouteTo){
+      if (this.breadcrumbList[pRouteTo].link) {
+        this.$router.push(this.breadcrumbList[pRouteTo].link);
+      }
+    },
+    updateList(){
+      this.breadcrumbList = this.$route.meta.breadcrumb;
+    } */
   },
+  /* watch: {
+    '$route' (){
+      this.breadcrumbList = this.$route.meta.breadcrumb;
+    }
+  }, */
+
 };
 </script>
 
 <style lang="scss" scoped>
 .header {
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
     z-index: 1;
   width: 100%;
   top: 0;
