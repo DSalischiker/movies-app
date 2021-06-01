a075aef906603aad300705e640eb75d268ea88a0
Diego <diegosalischiker711@gmail.com>
2021-06-01 15:15:49 -0300

    loadingState de los requests de data grandes a la API

--- src/store/index.js
+++ src/store/index.js
@@ -13,6 +13,7 @@ const store = new Vuex.Store({
     moviesByGenre: [],
     selectedMovieId: null,
     selectedMovieData: {},
+    isLoading: false,
     starRatingConfig: {
       increment: 0.1,
       starSize: 13,
@@ -44,17 +45,23 @@ const store = new Vuex.Store({
     setMovieData(state, movie) {
       state.selectedMovieData = movie;
     },
+    setLoadingState(state, loadingState){
+      state.isLoading = loadingState;
+    }
   },
   actions: {
     getMoviesFromAPI({ commit }) {
       return new Promise((resolve, reject) => {
+        commit("setLoadingState", true);
         API.get(`movie/popular?api_key=${process.env.VUE_APP_API_KEY}`)
           .then((response) => {
             commit("setMovies", response.data.results);
+            commit("setLoadingState", false);
             resolve(response);
           })
           .catch((error) => {
             console.error(error);
+            commit("setLoadingState", false);
             reject(error);
           });
       });
@@ -75,6 +82,7 @@ const store = new Vuex.Store({
     //COMPLETAR LLAMADO A API /discover enviando el id del género por params
     getMoviesByGenreFromAPI({commit, state}){
       return new Promise((resolve, reject) => {
+        commit("setLoadingState", true);
         API.get(`/discover/movie?api_key=${process.env.VUE_APP_API_KEY}`, {
           params: {
             with_genres: state.selectedGenreId,
@@ -82,23 +90,28 @@ const store = new Vuex.Store({
         })
         .then((response) => {
           commit("setMoviesByGenre", response.data.results);
+          commit("setLoadingState", false);
           resolve(response);
         })
         .catch((error) => {
           console.error(error);
+          commit("setLoadingState", false);
           reject(error);
         })
       })
     },
     getMovieDataFromAPI({ commit, state }) {
       return new Promise((resolve, reject) => {
+        commit("setLoadingState", true);
         API.get(`movie/${state.selectedMovieId}?api_key=${process.env.VUE_APP_API_KEY}`)
           .then((response) => {
             commit("setMovieData", response.data);
+            commit("setLoadingState", false);
             resolve(response);
           })
           .catch((error) => {
             console.error(error);
+            commit("setLoadingState", false);
             reject(error);
           });
       });
