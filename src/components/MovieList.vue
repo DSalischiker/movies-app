8770d2fd89598117af7d3dc5d46c45b63a7f22ff
Diego <diegosalischiker711@gmail.com>
2021-06-02 17:45:26 -0300

    Botones para ordenar movies[]
    Sort movies[] por Rating y Fecha de lanzamiento.
    Y otro botón para invertir el orden.
    (Fecha de lanzamiento comentado porque aún no funciona)

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="movie__list">
+    <div class="movie__list__buttons__container">
+      <button class="button" @click="sortByRating">
+        <i class="ri-star-line"></i>
+        Rating
+      </button>
+      <button class="button" @click="sortByPremiere">
+        <i class="ri-calendar-line"></i>
+        Lanzamiento
+      </button>
+      <button class="button" @click="reverseOrder">
+        <i class="ri-arrow-up-line"></i>
+        <i class="ri-arrow-down-line"></i>
+      </button>
+    </div>
     <ul v-if="moviesToShow != []" class="movie__list__grid">
       <MovieCard
         class="movie__list__ul__li"
@@ -15,6 +29,8 @@
 <script>
 // @ is an alias to /src
 import MovieCard from "../components/MovieCard.vue";
+import "remixicon/fonts/remixicon.css";
+
 export default {
   name: "MovieList",
   props: {
@@ -26,10 +42,40 @@ export default {
   components: {
     MovieCard,
   },
+  methods: {
+    sortByRating() {
+      this.movies.sort((a, b) => b.vote_average - a.vote_average);
+    },
+    //REVISAR
+    /* sortByPremiere() {
+      this.movies.sort(
+        (a, b) => {
+          if (this.getReleaseYear(b.release_date) - this.getReleaseYear(a.release_date)) {
+            return -1;
+          }else if(this.getReleaseYear(b.release_date) + this.getReleaseYear(a.release_date)){
+            return 1;
+          }
+          return 0;
+        }
+
+      );
+    }, */
+    getReleaseYear(releaseDate) {
+      const releaseYear = releaseDate.substring(0, 4);
+      return releaseYear;
+    },
+    reverseOrder(){
+      return this.movies.reverse();
+    },
+  },
   computed: {
     movies() {
       return this.moviesToShow;
     },
+    /* moviesByRating() {
+      return this.movies.sort((a, b) => a.vote_average + b.vote_average);
+
+    }, */
   },
 };
 </script>
@@ -38,9 +84,36 @@ export default {
 .movie__list {
   display: flex;
   width: 100%;
+  flex-direction: column;
+}
+.movie__list__buttons__container {
+  width: 95%;
+  display: flex;
+  justify-content: flex-end;
+  gap: 1em;
+  margin: 5vh 0;
+
+  .button {
+    display: flex;
+    gap: 0.5em;
+    align-items: center;
+    border: none;
+    color: $primaryColor;
+    background: none;
+    cursor: pointer;
+    font-size: 12px;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    padding: 0.5em 1em;
+    border-bottom: 2px solid white;
+    &:hover {
+      background-color: $primaryColorHover;
+      color: white;
+    }
+  }
 }
 .movie__list__grid {
-  margin: 10vh auto 0 auto;
+  margin: 0 auto;
   width: 90%;
   display: grid;
   grid-template-columns: repeat(5, 1fr);
