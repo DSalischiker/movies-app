5c2003d4c48f591165ac02400737926f99906dbd
Diego <diegosalischiker711@gmail.com>
2021-06-07 14:52:15 -0300

    MovieCard Overlay

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -8,6 +8,7 @@
   >
     <div class="movie-card__container" @click="selectMovie(movie.id)">
       <div class="movie-card__image__container">
+        <div class="content-overlay"></div>
         <img
           class="movie-card__img"
           :src="`https://image.tmdb.org/t/p/w500${this.movie.poster_path}`"
@@ -33,7 +34,7 @@
 
 <script>
 import StarRating from "vue-star-rating";
-import {mapMutations, mapGetters} from 'vuex';
+import { mapMutations, mapGetters } from "vuex";
 export default {
   name: "MovieCard",
   props: ["id", "movieData"],
@@ -52,9 +53,8 @@ export default {
       textClass: "rating-text",
     };
   },
-  methods:
-  mapMutations({
-    selectMovie: "setSelectedMovieId"
+  methods: mapMutations({
+    selectMovie: "setSelectedMovieId",
   }),
   computed: {
     movie() {
@@ -65,14 +65,14 @@ export default {
       return this.movieData.vote_average / 2;
     },
     ...mapGetters({
-      SRConfig: "getStarRatingConfig"
+      SRConfig: "getStarRatingConfig",
     }),
   },
 };
 </script>
 
 <style lang="scss" scoped>
-@import '../assets/mixins.scss';
+@import "../assets/mixins.scss";
 .movie-card {
   text-decoration: none;
 }
@@ -91,18 +91,48 @@ export default {
     transition: all ease-in-out 0.1s;
   }
 }
-.movie-card__image__container{
+.movie-card__image__container {
   @include movieCardImage(100%, 325px, $cardShadow, $movieCardBorder);
-  @media(max-width: $breakpointLaptop){
+  @media (max-width: $breakpointLaptop) {
     @include movieCardImage(100%, 275px, $cardShadow, $movieCardBorder);
   }
-  @media(max-width: $breakpointTablet){
+  @media (max-width: $breakpointTablet) {
     @include movieCardImage(100%, 375px, $cardShadow, $movieCardBorder);
   }
-  @media(max-width: $breakpointMobile){
+  @media (max-width: $breakpointMobile) {
     @include movieCardImage(100%, 400px, $cardShadow, $movieCardBorder);
   }
+  position: relative;
+  .content-overlay {
+    border-radius: $movieCardBorder;
+    background: rgba(255, 255, 255, 0.1);
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    opacity: 0;
+    -webkit-transition: all 0.1s ease-in-out 0s;
+    -moz-transition: all 0.1s ease-in-out 0s;
+    transition: all 0.1s ease-in-out 0s;
 
+    &:hover {
+      opacity: 1;
+    }
+  }
+
+  &:hover {
+      .content-overlay{
+        opacity: 1;
+      }
+      .content-details{
+        top: 50%;
+        left: 50%;
+        opacity: 1;
+      }
+    }
 }
 
 .movie-card__title {
